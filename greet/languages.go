289cefc37b54a8b48cecd4d034df27549a6ce8d3
adelson <tricker_bboy23@hotmail.com>
2023-02-17 18:19:53 -0500

    Initial commit

---
+++ greet/languages.go
@@ -0,0 +1,13 @@
+package greet
+
+import "fmt"
+
+func Spanish(name string) {
+	fmt.Println("Hola " + name)
+}
+func English(name string) {
+	fmt.Println("Hello " + name)
+}
+func Italian(name string) {
+	fmt.Println("Ciao " + name)
+}
